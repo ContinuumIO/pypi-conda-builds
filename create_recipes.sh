f9031dc2665cda6a78d241e4db05e914541bb2ff
Harsh Gupta <gupta.harsh96@gmail.com>
2015-05-05 13:26:12 +0530

    Improve create_script
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- create_recipes.sh
+++ create_recipes.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-rm list_of_recipies
-rm list_of_failed_recipies
+current_dir=`pwd`
+cd recipes
 while read line
 do
     package_name=$line
-    echo "Building conda recipe for $package_name"
+    echo "Creating conda recipe for $package_name"
     conda skeleton pypi $package_name
     if [ $? == 0 ]; then
-        echo "$package_name" >> list_of_recipies
+        echo "$package_name" >> ../list_of_recipies
     else
-        echo "$package_name" >> list_of_failed_recipies
+        echo "$package_name" >> ../list_of_failed_recipies
     fi
-done < $1
+done < $current_dir'/'$1
