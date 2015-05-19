83b07160a2ececa0b9c18bb11251a0599ce3fe9f
Harsh Gupta <gupta.harsh96@gmail.com>
2015-05-19 22:37:20 +0530

    Add arguments to list failed recipes and builds
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- calc_score.py
+++ calc_score.py
@@ -1,5 +1,11 @@
 from __future__ import print_function
+import argparse
 import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--list-failed-recipes", action="store_true")
+parser.add_argument("--list-failed-build", action="store_true")
+args = parser.parse_args()
 
 packages = yaml.load(file('packages.yaml', 'r'))
 
@@ -10,3 +16,12 @@ n = len(packages)
 
 print("recipe score: %s/%s" % (recipe_score, n))
 print("build score: %s/%s" % (build_score, n))
+
+if args.list_failed_recipes:
+    recipes = [package['name'] for package in packages
+               if package['recipe'] is not True]
+    print(recipes)
+
+if args.list_failed_recipes:
+    recipes = [package['name'] for package in packages if package['build'] is not True]
+    print(recipes)
