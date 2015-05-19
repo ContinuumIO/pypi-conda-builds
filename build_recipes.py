3c9e6dbdc9bd0aad1fbf899d3991d59f30f58bc5
Harsh Gupta <gupta.harsh96@gmail.com>
2015-05-19 22:50:50 +0530

    Add -r option to create_recipes and build_recipes
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- build_recipes.py
+++ build_recipes.py
@@ -1,6 +1,11 @@
 from __future__ import print_function
+import argparse
 import subprocess
 import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", help="re-attempt failed builds", action="store_true")
+args = parser.parse_args()
 
 packages = yaml.load(file('packages.yaml', 'r'))
 
@@ -12,7 +17,7 @@ build_log_file = open(log_dir + 'build_log', 'w')
 for package in packages:
     msg = "Creating Conda recipe for %s\n" % (package['name'])
     print(msg)
-    if package['build'] is None:
+    if package['build'] is None or args.r is True:
         # TODO: log packages which are built recursively
         err = subprocess.call(['conda', 'build', recipes_dir + package['name']],
                               stdout=build_log_file, stderr=build_log_file)
--- create_recipes.py
+++ create_recipes.py
@@ -1,7 +1,12 @@
 from __future__ import print_function
+from os.path import isdir
 import argparse
 import subprocess
 import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", help="re-attempt failed recipes", action="store_true")
+args = parser.parse_args()
 
 packages = yaml.load(file('packages.yaml', 'r'))
 
@@ -13,11 +18,22 @@ recipe_log_file = open(log_dir + 'recipe_log', 'w')
 for package in packages:
     msg = "Creating Conda recipe for %s\n" % (package['name'])
     print(msg)
-    if package['recipe'] is None:
-        # TODO: log recipes creation for
-        err = subprocess.call(['conda', 'skeleton', 'pypi', package['name'],
-                              '--output-dir', recipes_dir, '--recursive'],
-                              stdout=recipe_log_file, stderr=recipe_log_file)
+    cond = (package['recipe'] is None or
+            (args.r is True and package['recipe'] is False))
+    if cond:
+        if not isdir(recipes_dir + package['name']):
+            # XXX: the normalization of package names comes into way of
+            # directory detection
+            err = subprocess.call(['conda', 'skeleton', 'pypi',
+                                   package['name'],
+                                   '--output-dir',
+                                   recipes_dir,
+                                   '--recursive'],
+                                  stdout=recipe_log_file,
+                                  stderr=recipe_log_file)
+        else:
+            err = 0
+
         if err is 0:
             msg = "Succesfully created conda recipe for %s\n" % (package['name'])
             package['recipe'] = True
