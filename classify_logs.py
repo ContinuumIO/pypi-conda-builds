9b9619aef36d665b12818add16aebf9366172ab8
Harsh Gupta <gupta.harsh96@gmail.com>
2015-05-22 19:22:40 +0530

    Add more categories of failures
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- classify_logs.py
+++ classify_logs.py
@@ -2,6 +2,8 @@ import re
 import yaml
 
 error_types = ["no package found",
+               "missing build dependency",
+               "invalid syntax",
                "test failure",
                "unclassified"]
 
@@ -18,25 +20,35 @@ def no_packages_found(log):
     return any([re.match(p, line) for line in log])
 
 
-def split_build_and_test(log):
-    # XXX: This can be very memory inefficient
-    # Maybe don't even need to split the test and build parts
+# def split_build_and_test(log):
+#     # XXX: This can be very memory inefficient
+#     # Maybe don't even need to split the test and build parts
+#
+#     try:
+#         p = re.compile("TEST START")
+#         test_start = [re.match(p, line) is not None for line in log]
+#         start_index = test_start.index(True)
+#         return log[:start_index], log[start_index:]
+#     except ValueError:
+#         return log, []
 
-    try:
-        p = re.compile("TEST START")
-        test_start = [re.match(p, line) for line in log]
-        start_index = test_start.find(True)
-        return log[:start_index], log[start_index:]
-    except ValueError:
-        return log, []
 
-
-def has_test_failure(test_log):
+def has_test_failure(log):
     p = re.compile("TESTS FAILED")
-    return re.match(p, test_log[-1]) is not None
+    return re.match(p, log[-1]) is not None
 
 
-def classify_build_log(log_file):
+def has_missing_build_dependency(log):
+    p = re.compile("RuntimeError: Setuptools downloading is disabled")
+    return any([re.match(p, line) for line in log])
+
+
+def has_invalid_syntax(log):
+    p = re.compile("SyntaxError: invalid syntax")
+    return any([re.match(p, line) for line in log])
+
+
+def classify_build_log(log_file, package):
     """
     Takes a build log file object as an input and returns
     a tupe `(category, sub-category, sub-category)`
@@ -46,14 +58,21 @@ def classify_build_log(log_file):
         - Test Dependency
     - Runtime error (other than missing dependency)
     """
+    if package['recipe'] is False:
+        return "No recipe available"
+
     log = log_file.readlines()
     if no_packages_found(log):
         return "no package found"
 
-    build, test = split_build_and_test(log)
-    if test:
-        if has_test_failure(test):
-            return "test failure"
+    if has_test_failure(log):
+        return "test failure"
+
+    if has_missing_build_dependency(log):
+        return "missing build dependency"
+
+    if has_invalid_syntax(log):
+        return "invalid syntax"
 
     return "unclassified"
 
@@ -66,7 +85,7 @@ def classify_all_logs():
         if package['build'] is False:
             log_file_name = log_dir + "%s_build.log" % (package['name'])
             log_file = open(log_file_name, 'r')
-            error_type = classify_build_log(log_file)
+            error_type = classify_build_log(log_file, package)
         else:
             error_type = None
         package['build_error_type'] = error_type
@@ -75,4 +94,6 @@ def classify_all_logs():
 
 
 if __name__ == "__main__":
+    packages = yaml.load(file('packages.yaml', 'r'))
     classify_all_logs()
+    package_dict = dict([(package['name'], i) for i, package in enumerate(packages)])
